1112ab5bde01d27a3bf1ffe3f8911ae57e892b02
Paul Pineda <paul@pineda.codes>
2017-07-15 00:50:18 +0100

    Amended the update method so the current ball positions were caluclated with plus rather than mltiplication (the latter produced exponential speed increases)

--- src/index.js
+++ src/index.js
@@ -25,8 +25,6 @@ function drawBall(ctx, ball){
   ctx.fill();
   ctx.arc(ball.x, ball.y, ball.rad, 0, 2*Math.PI, true);
   ctx.fill();
-
-  console.log('bal created at', ball.x, ball.y);
 }
 
 // ==================================
@@ -39,10 +37,12 @@ const ctx = canvas.getContext('2d');
 // ============================================
 // UPDATE THE CANVAS WITH requestAnimationFrame
 // ============================================
+const gravity = 9.8;
+const friction = 0.9;
 const BALL_POOL = new BallPool();
 
 function frame(){
-
+  ctx.clearRect(0,0,400,400);
   BALL_POOL.update();
   BALL_POOL.render();
   requestAnimationFrame(frame);
@@ -59,10 +59,21 @@ function BallPool(){
 BallPool.prototype.createBall = function(x, y){
   this.balls.push(new Ball(x, y));
 };
-BallPool.prototype.update = function(){
+BallPool.prototype.update = function(dt){
   this.balls.forEach(ball => {
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+    ball.x = ball.vx * dt;
+    ball.y = ball.vy * dt;
+
+    const overflowX = canvas.width - ball.x - ball.rad;
+    const overflowY = canvas.height - ball.y - ball.rad;
+    if(overflowY < 0){
+      ball.y = canvas.height + overflowY;
+      ball.vy = overflowY * friction;
+    }
+    if(ball.y < ball.rad){
+      ball.y = -ball.y
+      ball.vy =  ball.vy * friction;
+    }
   });
 };
 BallPool.prototype.render = function(){
@@ -76,4 +87,5 @@ function Ball(x, y){
   this.y = y;
   this.vx = 0;
   this.vy = 1;
+  console.log('NEW BALL V:',this.vx,this.vy);
 }
